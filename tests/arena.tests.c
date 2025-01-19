9a6a4556ead07965f512b42d6c336ad5f7475e7c
ilmartoo <martinszga@gmail.com>
2025-01-19 18:51:48 +0000

    Added some arena tests

--- tests/arena.tests.c
+++ tests/arena.tests.c
@@ -1,3 +1,15 @@
 #include "arena.h"
+#include "test.h"
 
-void ArenaTests(void) {}
+void ArenaTests(void)
+{
+    TestGroup("arena.h arena.c", {
+        TestCase("Zero intialization", {
+            size_t size = 400;
+            Arena arena = ArenaCreate();
+            void* abc = ArenaPushZero(&arena, size);
+
+            AssertMemZero("The reserved space should be initialized to 0", abc, size);
+        });
+    });
+}
