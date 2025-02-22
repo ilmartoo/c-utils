96897728d8e9364e7527113b04af2f08d6568e8e
ilmartoo <martinszga@gmail.com>
2025-02-22 19:53:30 +0100

    Updated memory section

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -2,9 +2,7 @@
 #ifndef __ILMARTO_MEMORY_H_
 #define __ILMARTO_MEMORY_H_
 
-#ifndef size_t
-typedef unsigned long long size_t;
-#endif
+#include <stddef.h>
 
 /**
  * Copies the specified number of bytes from a source to a destination.
@@ -22,5 +20,20 @@ void *memory_copy(void *dest, const void *src, size_t bytes);
  * @return Reference to the destination memory space.
  */
 #define memory_copy_type(dest, src, type) ((type *)memory_copy(dest, src, sizeof(type)))
+
+/**
+ * Sets the specified number of bytes from a memory space to 0.
+ * @param memory Memory space.
+ * @param bytes Number of bytes update.
+ * @return Reference to the memory space.
+ */
+void *memory_zero(void *memory, size_t bytes);
+/**
+ * Sets the type contained in the memory space to 0.
+ * @param memory Memory space.
+ * @param type Type to set to 0.
+ * @return Reference to the memory space.
+ */
+#define memory_zero_type(memory, type) ((type *)memory_zero(memory, sizeof(type)))
 
 #endif // __ILMARTO_MEMORY_H_
